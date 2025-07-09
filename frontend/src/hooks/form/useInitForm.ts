f5c6c06f2faf52001e713f6e7f6ab93a3d96bcb8
Cedrik Hoffmann <dev@choffmann.io>
2025-07-10 00:55:14 +0200

    chore: remove unused form hook

--- frontend/src/hooks/form/useInitForm.ts
+++ frontend/src/hooks/form/useInitForm.ts
@@ -1,6 +1,4 @@
-import { useEffect, useRef } from 'react'
 import { useSuspenseQuery, UseSuspenseQueryOptions } from '@tanstack/react-query'
-import useFormStore, { FormStore } from '@/store/form/useFormStore'
 
 export const useInitFormQuery = <TData, TSchema>(
   queryOptions: UseSuspenseQueryOptions<TData>,
@@ -8,22 +6,4 @@ export const useInitFormQuery = <TData, TSchema>(
 ) => {
   const { data } = useSuspenseQuery(queryOptions)
   return { initForm: handler(data), loadedData: data }
-}
-
-export const useInitForm = <T>(defaulForm: T) => {
-  const { init, isEmpty, form } = useFormStore((state: FormStore<T>) => ({
-    init: state.commit,
-    isEmpty: state.isEmpty,
-    form: state.form,
-  }))
-  const initForm = useRef<T | undefined>(form)
-
-  useEffect(() => {
-    if (isEmpty()) {
-      initForm.current = defaulForm
-      init(initForm.current)
-    }
-  }, [defaulForm, init, isEmpty])
-
-  return { initForm: initForm.current }
 }
