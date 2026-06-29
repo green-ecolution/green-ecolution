285b92fc84e3086d78a8d46804faa917002552da
Cedrik Hoffmann <choffmann@progeek.de>
2026-06-29 12:17:19 +0200

    test(sensor): add failing relink/deactivate integration tests

--- backend-rs/crates/server/test/api/main.rs
+++ backend-rs/crates/server/test/api/main.rs
@@ -14,6 +14,7 @@ pub mod sensor_activate;
 pub mod sensor_create;
 pub mod sensor_models;
 pub mod sensor_mqtt;
+pub mod sensor_relink;
 pub mod sensor_status;
 pub mod sensors;
 pub mod statistics;
